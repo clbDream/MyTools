159b974a54792d5e29a7eb590602a1b45339843c
clbDream <17600686054@163.com>
2017-09-05 19:30:59 +0800

    上传网络工具类：判断当前是否为WIfi、当前网络是否可用

--- app/src/main/java/com/clb/mytools/MainActivity.java
+++ app/src/main/java/com/clb/mytools/MainActivity.java
@@ -3,23 +3,22 @@ package com.clb.mytools;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.clb.mytoolslibrary.ToastUtils;
+import com.clb.mytoolslibrary.VersionUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvVersionCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /**
-     * 测试吐司
-     * @param view
-     */
-    public void toast(View view) {
-
-        ToastUtils.showToast(this,"弹出吐司成功");
+        //获取应用的版本号
+        tvVersionCode = (TextView) findViewById(R.id.tv_version_code);
+        tvVersionCode.setText(VersionUtils.getAppVersionCode(this)+"");
     }
 }
